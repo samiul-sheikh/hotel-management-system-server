812c309fc69a58a5a439c8498b0c59ee0a3ec38a
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-28 03:52:59 +0600

    send data to database and store in mongodb

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const port = 8000;
 
-const password = "burjAdmin71";
+const app = express()
 
+app.use(cors());
+app.use(bodyParser.json());
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://burjAdmin:burjAdmin71@cluster0.2uohe.mongodb.net/burjAlArab?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("burjAlArab").collection("bookings");
-    console.log('database connected successfully');
-    client.close();
+    const bookings = client.db("burjAlArab").collection("bookings");
+
+    // create/store data in database
+    app.post('/addBooking', (req, res) => {
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+            .then(result => {
+                // console.log(result);
+                res.send(result.insertedCount > 0);
+            })
+        // console.log(newBooking);
+    })
 });
 
 app.get('/', (req, res) => {
