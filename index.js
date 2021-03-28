31c0336dce279d60563bae982aa77da2f4d0b967
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-28 23:17:34 +0600

    verify jwt token in the server end using firebase admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require('firebase-admin');
 
 const port = 8000;
 
@@ -8,6 +9,12 @@ const app = express()
 
 app.use(cors());
 app.use(bodyParser.json());
+
+var serviceAccount = require("./hotel-management-burj-firebase-adminsdk-1p4rq-0b11d75391.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://burjAdmin:burjAdmin71@cluster0.2uohe.mongodb.net/burjAlArab?retryWrites=true&w=majority";
@@ -29,11 +36,29 @@ client.connect(err => {
     // read data from database and display UI
     app.get('/bookings', (req, res) => {
         // console.log(req.query.email);
-        console.log(req.headers.authorization);
-        bookings.find({ email: req.query.email })
-            .toArray((err, documents) => {
-                res.send(documents);
-            })
+        // console.log(req.headers.authorization);
+        const bearer = req.headers.authorization;
+        if (bearer && bearer.startsWith('Bearer ')) {
+            const idToken = bearer.split(' ')[1];
+            console.log({ idToken });
+            admin
+                .auth()
+                .verifyIdToken(idToken)
+                .then((decodedToken) => {
+                    const tokenEmail = decodedToken.email;
+                    const queryEmail = req.query.email;
+                    console.log(tokenEmail, queryEmail);
+                    if (tokenEmail == req.query.email) {
+                        bookings.find({ email: req.query.email })
+                            .toArray((err, documents) => {
+                                res.send(documents);
+                            })
+                    }
+                })
+                .catch((error) => {
+                    // Handle error
+                });
+        }
     })
 });
 
